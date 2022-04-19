2daf6e69f779af11c38b14ca807395ec9f9d6326
Coen Meulenkamp (Scribbd) <coenmeulenkamp@gmail.com>
2022-04-19 17:35:19 +0200

    pip module not found fixed

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 # Setup virtual environent
 python -m venv .venv
-python -m pip install -t requirements.txt
+pip install -t requirements.txt
 # Activate virtual environment
 ./.venv/Scripts/Activate.ps1
